19af584b36299cde15a7528092f0199ac89ee1f8
yerzat <erzatnis@gmail.com>
2022-06-19 14:32:37 +0600

    first commit

---
+++ src/main/java/kz/meirambekuly/skidkilife/SkidkiLifeApplication.java
@@ -0,0 +1,13 @@
+package kz.meirambekuly.skidkilife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkidkiLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkidkiLifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/meirambekuly/skidkilife/SkidkiLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.meirambekuly.skidkilife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkidkiLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
