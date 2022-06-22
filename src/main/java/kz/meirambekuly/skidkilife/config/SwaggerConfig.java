e8259e8bd6919c8b6ed9b9ce959123ce876ed727
yerzat <erzatnis@gmail.com>
2022-06-22 11:50:01 +0600

    created configurations (jwt, security and swagger)

---
+++ src/main/java/kz/meirambekuly/skidkilife/config/SwaggerConfig.java
@@ -0,0 +1,59 @@
+package kz.meirambekuly.skidkilife.config;
+
+import kz.meirambekuly.skidkilife.utilities.Constants;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(new ApiKey(Constants.PREFIX,
+                        Constants.AUTHORIZATION_PREFIX, Constants.PREFIX + " ")))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("kz.meirambekuly.skidkilife"))
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot REST API",
+                "Spring Boot REST API for Skidki.Life Project",
+                "1.0",
+                "Terms of service",
+                "Meirambekuly Yerzat",
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0");
+        return apiInfo;
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/.*"))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference(Constants.PREFIX, authorizationScopes));
+    }
+}
---
+++ src/main/java/kz/meirambekuly/skidkilife/web/dto/RoleDto.java
@@ -0,0 +1,11 @@
+package kz.meirambekuly.skidkilife.web.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class RoleDto {
+    private Long id;
+    private String name;
+}
