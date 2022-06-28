381fbe00e27e0fb0c54123df6cada2d8f19c6900
yerzat <erzatnis@gmail.com>
2022-06-28 15:32:08 +0600

    Controllers for establishment and sale are creaeted

---
+++ src/main/java/kz/meirambekuly/skidkilife/web/rest/AuthController.java
@@ -0,0 +1,62 @@
+package kz.meirambekuly.skidkilife.web.rest;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import kz.meirambekuly.skidkilife.config.jwt.Jwt;
+import kz.meirambekuly.skidkilife.constants.Errors;
+import kz.meirambekuly.skidkilife.services.EstablishmentService;
+import kz.meirambekuly.skidkilife.utilities.Constants;
+import kz.meirambekuly.skidkilife.web.dto.ResultDto;
+import kz.meirambekuly.skidkilife.web.dto.establishmentDtos.EstablishmentCreatorDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(Constants.PUBLIC_ENDPOINT + "/auth")
+@RequiredArgsConstructor
+@ApiModel(value = "Auth Controller", description = "Authentication Controller")
+public class AuthController {
+
+    private final EstablishmentService establishmentService;
+    private final AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/register")
+    @ApiOperation(value = "user registration")
+    public ResponseEntity<?> register (@ApiParam(value = "DTO for user registration") @RequestBody EstablishmentCreatorDto dto){
+        return ResponseEntity.ok(establishmentService.register(dto));
+    }
+
+    @PostMapping("/login")
+    @ApiOperation(value = "user login")
+    public ResponseEntity<?> login(@RequestParam("phoneNumber") String phoneNumber,
+                                   @RequestParam("password") String password){
+        if(phoneNumber.isEmpty() && password.isEmpty()){
+            throw new RuntimeException("Not Valid Fields");
+        }
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(phoneNumber, password));
+        if (authentication.isAuthenticated()) {
+            String token = Jwt.generateJwt(authentication.getName(), authentication.getAuthorities());
+            return ResponseEntity.ok(ResultDto.builder()
+                    .isSuccess(true)
+                    .HttpStatus(HttpStatus.OK.value())
+                    .data(token)
+                    .build());
+        }
+        return ResponseEntity.ok(ResultDto.builder()
+                .isSuccess(false)
+                .HttpStatus(HttpStatus.UNAUTHORIZED.value())
+                .errorMessage(Errors.UNAUTHORIZED)
+                .build());
+    }
+
+
+
+}
--- src/main/java/kz/meirambekuly/skidkilife/web/rest/EstablishmentController.java
+++ src/main/java/kz/meirambekuly/skidkilife/web/rest/EstablishmentController.java
@@ -1,9 +1,12 @@
 package kz.meirambekuly.skidkilife.web.rest;
 
+import kz.meirambekuly.skidkilife.services.EstablishmentService;
 import kz.meirambekuly.skidkilife.utilities.Constants;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,12 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EstablishmentController {
 
+    private final EstablishmentService establishmentService;
 
-
-    @GetMapping
-    public String sayHello(){
-        return "Hello";
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAllEstablishments(){
+        return ResponseEntity.ok(establishmentService.findAll());
     }
 
+    @GetMapping("/findByName")
+    public ResponseEntity<?> findAllByName(@RequestParam("name") String name){
+        return ResponseEntity.ok(establishmentService.findAllByName(name));
+    }
+
+    @GetMapping("/findByAddress")
+    public ResponseEntity<?> findAllByAddress(@RequestParam("address") String address){
+        return ResponseEntity.ok(establishmentService.findByAddress(address));
+    }
+
+    @GetMapping("/findByType")
+    public ResponseEntity<?> findAllByType(@RequestParam("type") String type){
+        return ResponseEntity.ok(establishmentService.findByType(type));
+    }
+
+    @GetMapping("/findById")
+    public ResponseEntity<?> findById(@RequestParam("id") Long id){
+        return ResponseEntity.ok(establishmentService.findById(id));
+    }
 
 }
